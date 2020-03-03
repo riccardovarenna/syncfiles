5ea340391b7ff42cf5cfd6f00a3f3d772e98baf3
Riccardo <riccardovarenna@hotmail.de>
2020-03-03 01:20:17 +0100

    map spc spc to m-x

--- dotfiles/doom/config.el
+++ dotfiles/doom/config.el
@@ -3,6 +3,8 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+(map! :leader "SPC" #'execute-extended-command)
+
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Riccardo Varenna"
